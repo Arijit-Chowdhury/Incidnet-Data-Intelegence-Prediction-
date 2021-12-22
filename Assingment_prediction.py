74d59b84d4654e757a723e3bf5af32fee66a264a
Arijit-Chowdhury <96465438+Arijit-Chowdhury@users.noreply.github.com>
2021-12-22 20:30:03 +0530

    Add files via upload

---
+++ Assingment_prediction.py
@@ -0,0 +1,92 @@
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import numpy as np
+import json
+import nltk
+import re
+
+filename = 'finalized_model.sav'
+def clean_text(text):
+    # remove backslash-apostrophe 
+    text = re.sub("\'", "", text) 
+    # remove everything except alphabets 
+    text = re.sub("[^a-zA-Z]"," ",text) 
+    # remove whitespaces 
+    text = ' '.join(text.split()) 
+    # convert text to lowercase 
+    text = text.lower() 
+    return text
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+def remove_stopwords(text):
+    no_stopword_text = [w for w in text.split() if not w in stop_words]
+    return ' '.join(no_stopword_text)
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    print (request.is_json)
+    content = request.get_json()
+    q=(content['Description'])
+    print (q)
+    loaded_model = pickle.load(open(filename, 'rb'))
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=1000)
+    q = clean_text(q)
+    q = remove_stopwords(q)
+    loaded_vectorizer = pickle.load(open('vectorizer.pickle', 'rb'))
+    q_vec = loaded_vectorizer.transform([q])
+    prediction = loaded_model.predict(q_vec)
+    loaded_service = pickle.load(open('finalized_model_service.sav', 'rb'))
+    service=loaded_service.predict(q_vec)
+    output = '[{"Assignment Group":"'+prediction[0]+'","Service":"'+service[0]+'"}]'
+    output=json.loads(output)
+    import pypyodbc    
+    from datetime import datetime
+    connection = pypyodbc.connect('Driver={SQL Server Native Client 11.0};Server=ftlqsmasql01;Database=SystemData;Trusted_Connection=yes;')    
+    alert_desc = content['Description']   
+    login_time= datetime.now()
+    Source = request.remote_addr
+    assignment_group = prediction[0]
+    service_name = service[0]
+    cursor = connection.cursor()   
+    SQLCommand = ("INSERT INTO model_access_log(access_time,alert_description,source_ip,assingment_group,service) VALUES(?,?,?,?,?)")    
+    Values = [login_time,alert_desc,Source,assignment_group,service_name]   
+    
+    #Processing Query    
+    cursor.execute(SQLCommand,Values)     
+    #Commiting any pending transaction to the database.    
+    connection.commit()    
+    #closing connection    
+    print("Data Successfully Inserted")   
+    connection.close()    
+
+    return jsonify(output)
+    
+
+if __name__ == "__main__":
+    #app.run(debug=True)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=5080)
+    app.run(host='0.0.0.0', port= 5000)
---
+++ model.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+import json
+import nltk
+import re
+import csv
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import pickle
+
+pd.set_option('display.max_colwidth', 300)
+meta = pd.read_csv("D:\ML\inc_1.csv", sep = ',', header = None,encoding='latin-1')
+#meta.head()
+# rename columns
+meta.columns = ["incident_num","description","assignment_group",3]
+#meta.head()
+
+inc = pd.DataFrame(meta)
+
+incident=inc[['incident_num','description','assignment_group']]
+incident.head()
+assigns = [] 
+
+# extract genres
+for i in incident['assignment_group']: 
+  assigns.append(list(i.split(",")))
+
+
+#print(assigns)
+all_assign = sum(assigns,[])
+len(set(all_assign))
+
+all_assign = nltk.FreqDist(all_assign) 
+
+# create dataframe
+all_assign_df = pd.DataFrame({'assigns': list(all_assign.keys()), 
+                              'Count': list(all_assign.values())})
+all_assign_df.head()
+g = all_assign_df.nlargest(columns="Count", n = 50) 
+plt.figure(figsize=(12,15)) 
+ax = sns.barplot(data=g, x= "Count", y = "assigns") 
+ax.set(ylabel = 'Count') 
+#plt.show()
+# function for text cleaning 
+def clean_text(text):
+    # remove backslash-apostrophe 
+    text = re.sub("\'", "", text) 
+    # remove everything except alphabets 
+    text = re.sub("[^a-zA-Z]"," ",text) 
+    # remove whitespaces 
+    text = ' '.join(text.split()) 
+    # convert text to lowercase 
+    text = text.lower() 
+    
+    return text
+pd.options.mode.chained_assignment = None 
+incident['description']=incident['description'].apply(str)
+incident['clean_desc'] = incident['description'].apply(lambda x: clean_text(x))
+
+def freq_words(x, terms = 30): 
+  all_words = ' '.join([text for text in x]) 
+  all_words = all_words.split() 
+  fdist = nltk.FreqDist(all_words) 
+  words_df = pd.DataFrame({'word':list(fdist.keys()), 'count':list(fdist.values())}) 
+  
+  # selecting top 20 most frequent words 
+  d = words_df.nlargest(columns="count", n = terms) 
+  
+  # visualize words and frequencies
+  plt.figure(figsize=(12,15)) 
+  ax = sns.barplot(data=d, x= "count", y = "word") 
+  ax.set(ylabel = 'Word') 
+ # plt.show()
+  
+# print 100 most frequent words 
+freq_words(incident['clean_desc'], 100)
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+# function to remove stopwords
+def remove_stopwords(text):
+    no_stopword_text = [w for w in text.split() if not w in stop_words]
+    return ' '.join(no_stopword_text)
+
+incident['clean_desc'] = incident['clean_desc'].apply(lambda x: remove_stopwords(x))
+from sklearn.preprocessing import MultiLabelBinarizer
+
+multilabel_binarizer = MultiLabelBinarizer()
+multilabel_binarizer.fit(incident['assignment_group'])
+
+# transform target variable
+y = multilabel_binarizer.transform(incident['assignment_group'])
+#print (multilabel_binarizer)
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=1000)
+# split dataset into training and validation set
+xtrain, xval, ytrain, yval = train_test_split(incident['clean_desc'], incident['assignment_group'], test_size=0.05, random_state=1)
+# create TF-IDF features
+xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
+xval_tfidf = tfidf_vectorizer.transform(xval)
+
+vec_file = 'vectorizer.pickle'
+pickle.dump(tfidf_vectorizer, open(vec_file, 'wb'))
+
+from sklearn.linear_model import LogisticRegression
+
+# Binary Relevance
+from sklearn.multiclass import OneVsRestClassifier
+
+# Performance metric
+from sklearn.metrics import f1_score
+lr = LogisticRegression(solver='lbfgs', max_iter=10000)
+clf = OneVsRestClassifier(lr)
+# fit model on train data
+clf.fit(xtrain_tfidf, ytrain)
+
+filename = 'finalized_model.sav'
+pickle.dump(clf, open(filename, 'wb'))
+
+# make predictions for validation set
+y_pred = clf.predict(xval_tfidf)
+#y_pred[3]
+# evaluate performance
+f1_score(yval, y_pred, average="micro")
+# predict probabilities
+y_pred_prob = clf.predict_proba(xval_tfidf)
+# Saving model to disk
+pickle.dump(lr, open('model.pkl','wb'))
